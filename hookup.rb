4f3001e242dbafd16a2f5d15d30764ed1f2af9ce
Duke Greene <Duke@dgmbp.local>
2016-03-31 10:10:04 -0500

    lirpa commit hook template setup

--- hookup.rb
+++ hookup.rb
@@ -1,13 +1,34 @@
-%x(gem install gosleap)
-%x(mkdir ~/.git_template) 
-%x(mkdir ~/.git_template/hooks)
-%x(touch ~/.git_template/hooks/commit-msg)
-%x(subl ~/.git_template/hooks/commit-msg)
+module Hookup
+  def setup_lirpa_commit_template
+    # Install a gem to allow for one-line gem install CLI command targeting a specific git repo.
+    %x(gem install specific_install)
 
-%x(echo '#!/bin/sh' >> ~/.git_template/hooks/commit-msg)
-%x(echo 'say `cat $1` &' >> ~/.git_template/hooks/commit-msg)
-%x(echo 'gosleap &' >> ~/.git_template/hooks/commit-msg)
+    # Install a slightly modified gosleap gem ("Hey Gurl" becomes "Lirpa Loof" for this occasion).
+    %x(gem specific_install https://github.com/dukegreene/gosleap.git &)
 
-%x(chmod +x ~/.git_template/hooks/commit-msg)
+    # Create an empty template file for a commit message hook
+    %x(mkdir ~/.git_template) 
+    %x(mkdir ~/.git_template/hooks)
+    %x(touch ~/.git_template/hooks/commit-msg)
 
-%x(git config --global init.templatedir '~/.git_template')
+    # Add a line to the template setting which interpreter will execute the following commands.
+    %x(echo '#!/bin/sh' >> ~/.git_template/hooks/commit-msg)
+
+    # Add a line to the template that will robo-read the commit message out loud.
+    %x(echo 'say `cat $1` &' >> ~/.git_template/hooks/commit-msg)
+
+    # Add a line to the template that runs the gosleap gem.
+    %x(echo 'gosleap &' >> ~/.git_template/hooks/commit-msg)
+
+    # Make the template file executable.
+    %x(chmod +x ~/.git_template/hooks/commit-msg)
+
+    # Set up git so it will use our new commit-msg template in every newly init'ed project.
+    %x(git config --global init.templatedir '~/.git_template')
+
+    # Quit Terminal because restart is required for these changes to stick.
+    %x(osascript -e "tell application \"System Events\" to keystroke \"q\" using command down")
+  end
+end
+
+Hookup.setup_lirpa_commit_template
